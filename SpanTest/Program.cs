40ac333a8d02635b4fafd86809551af092e7fac2
Johnny Qian <me@johnnyqian.net>
2022-04-22 09:39:17 +0800

    Initial commit

---
+++ SpanTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<TestSpan>();
+
+/// <summary>
+/// Span<T>
+/// https://mp.weixin.qq.com/s/K2YkvQnPXn6gWtICS8Zz_A
+/// </summary>
+public class TestSpan
+{
+    [Benchmark]
+    public void Demo1()
+    {
+        var arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        for (var i = 0; i < arr.Length; i++)
+        {
+            arr[i] += 1;
+        }
+    }
+
+    [Benchmark]
+    public void Demo2()
+    {
+        var arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        foreach (ref var i in arr.AsSpan())
+        {
+            i++;
+        }
+    }
+}
---
+++ ValueTaskTest/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Net.Http.Headers;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json.Linq;
+
+BenchmarkRunner.Run<TestValueTask>();
+
+/// <summary>
+/// Task vs ValueTask: When Should I use ValueTask?
+/// https://www.youtube.com/watch?v=dCj7-KvaIJ0
+/// https://www.youtube.com/watch?v=mEhkelf0K6g
+/// </summary>
+[MemoryDiagnoser]
+public class TestValueTask
+{
+    private static readonly GitHubService Service = new();
+
+    [Benchmark]
+    public async Task<object> RunTask()
+    {
+        return await Service.GetReposAsyncTask("johnnyqian");
+    }
+
+    [Benchmark]
+    public async Task<object> RunValueTask()
+    {
+        return await Service.GetReposAsyncValueTask("johnnyqian");
+    }
+}
+
+public class GitHubService
+{
+    private readonly IMemoryCache cacheRepos = new MemoryCache(new MemoryCacheOptions());
+    private readonly HttpClient httpClient = new HttpClient();
+
+    public GitHubService()
+    {
+        // 403 (Forbidden) error if not add this line
+        httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("TaskVsValueTask", "1.0"));
+    }
+
+    public async Task<List<JObject>> GetReposAsyncTask(string username)
+    {
+        var repos = cacheRepos.Get<List<JObject>>(username);
+        if (repos is null)
+        {
+            var response = await httpClient.GetAsync($"https://api.github.com/users/{username}/repos");
+            response.EnsureSuccessStatusCode();
+            repos = await response.Content.ReadAsAsync<List<JObject>>();
+            cacheRepos.Set(username, repos, TimeSpan.FromHours(1));
+            Console.WriteLine("Get repos from GitHub with AsyncTask");
+        }
+        return repos;
+    }
+
+    public async ValueTask<List<JObject>> GetReposAsyncValueTask(string username)
+    {
+        var repos = cacheRepos.Get<List<JObject>>(username);
+        if (repos is null)
+        {
+            var response = await httpClient.GetAsync($"https://api.github.com/users/{username}/repos");
+            response.EnsureSuccessStatusCode();
+            repos = await response.Content.ReadAsAsync<List<JObject>>();
+            cacheRepos.Set(username, repos, TimeSpan.FromHours(1));
+            Console.WriteLine("Get repos from GitHub with AsyncValueTask");
+        }
+        return repos;
+    }
+}
